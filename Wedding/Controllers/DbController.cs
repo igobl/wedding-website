7b385503ecbde57cec6f775f5968ecc01a188668
Ian Göbl <ian.gobl@flipdish.com>
2024-03-05 21:54:46 +0000

    Add endpoint to run migrations.
    Not ideal, but automatically applying migrations in github actions is not working

---
+++ Wedding/Controllers/DbController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wedding.ef;
+
+namespace Wedding.Controllers
+{
+    public class DbController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DbController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            _context.Database.Migrate();
+            return Json("Migrations Complete");
+        }
+    }
+}
